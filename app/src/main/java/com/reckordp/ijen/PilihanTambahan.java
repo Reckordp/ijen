abb00320f50a30f15c2132206043d4d2e81e3ae1
Reckordp <rmuhammad134@gmail.com>
2022-03-04 16:32:06 +0700

    Implement expand view

--- app/src/main/java/com/reckordp/ijen/PilihanTambahan.java
+++ app/src/main/java/com/reckordp/ijen/PilihanTambahan.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -21,8 +22,10 @@ import java.util.ArrayList;
 public class PilihanTambahan extends Fragment {
 
     private final int INDIKATOR_PANEL_ANGLE = 180;
+    private boolean expanded = false;
     private boolean hasStarted = false;
     private int selected = -1;
+    private int totalPilihanHeight;
     private ViewGroup panel;
 
 
@@ -37,16 +40,33 @@ public class PilihanTambahan extends Fragment {
         return panel;
     }
 
+    // We expand the list if expanded is false
+    void animationForPilihan(float interpolatedTime) {
+        ListView pilihan = panel.findViewById(R.id.pilihan_jumlah);
+        ViewGroup.LayoutParams params = pilihan.getLayoutParams();
+        if (expanded) {
+            params.height = (int)(totalPilihanHeight * (1.0 - interpolatedTime));
+        } else {
+            params.height = (int)(totalPilihanHeight * interpolatedTime);
+        }
+        pilihan.setLayoutParams(params);
+        pilihan.invalidate();
+        pilihan.requestLayout();
+    }
+
     void attachAksiPanelOnClickListener() {
         FloatingActionButton aksiPanel = panel.findViewById(R.id.aksi_panel_tambah);
         aksiPanel.setOnClickListener(v -> {
             int origin = aksiPanel.getRotation() == 0 ? 0 : INDIKATOR_PANEL_ANGLE;
+            Button tombolBayar = panel.findViewById(R.id.bayar);
+
             Animation anim = new Animation() {
                 @Override
                 protected void applyTransformation(float interpolatedTime, Transformation t) {
                     super.applyTransformation(interpolatedTime, t);
                     float angle = origin + INDIKATOR_PANEL_ANGLE * interpolatedTime;
                     aksiPanel.setRotation((int)(angle % 360));
+                    if (hasStarted) animationForPilihan(interpolatedTime);
                 }
             };
             anim.setDuration(200);
@@ -54,11 +74,14 @@ public class PilihanTambahan extends Fragment {
                 @Override
                 public void onAnimationStart(Animation animation) {
                     hasStarted = true;
+                    if (expanded) tombolBayar.setVisibility(View.GONE);
                 }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
                     hasStarted = false;
+                    expanded = aksiPanel.getRotation() != 0;
+                    if (expanded) tombolBayar.setVisibility(View.VISIBLE);
                 }
 
                 @Override
@@ -83,9 +106,10 @@ public class PilihanTambahan extends Fragment {
                 deretPilihan
         );
         pilihan.setAdapter(adapter);
-
         pilihan.setOnItemClickListener((parent, view, position, id) -> selected = position);
-        panel.findViewById(R.id.bayar).setOnClickListener(v -> {
+
+        Button bayar = panel.findViewById(R.id.bayar);
+        bayar.setOnClickListener(v -> {
             if (selected != -1) {
                 MainActivity activity = (MainActivity) getActivity();
                 String dipilih = deretPilihan.get(selected);
@@ -94,5 +118,19 @@ public class PilihanTambahan extends Fragment {
                 activity.pembayaranDipilih(Integer.parseInt(dipilih));
             }
         });
+
+        pilihan.measure(0, 0);
+        totalPilihanHeight = pilihan.getMeasuredHeight() * deretPilihan.size();
+        totalPilihanHeight += pilihan.getDividerHeight() * (deretPilihan.size() - 1);
+        Log.i("UUU", String.valueOf(totalPilihanHeight));
+
+        if (!expanded) {
+            ViewGroup.LayoutParams params = pilihan.getLayoutParams();
+            params.height = 0;
+            pilihan.setLayoutParams(params);
+            pilihan.invalidate();
+            pilihan.requestLayout();
+            bayar.setVisibility(View.GONE);
+        }
     }
 }
