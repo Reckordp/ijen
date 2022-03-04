b31b386d5578cf2c45857c3d12f8957320dd5773
Reckordp <rmuhammad134@gmail.com>
2022-03-04 16:03:28 +0700

    Refactor

--- app/src/main/java/com/reckordp/ijen/PilihanTambahan.java
+++ app/src/main/java/com/reckordp/ijen/PilihanTambahan.java
@@ -7,10 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -20,23 +17,27 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class PilihanTambahan extends Fragment {
 
     private final int INDIKATOR_PANEL_ANGLE = 180;
     private boolean hasStarted = false;
     private int selected = -1;
+    private ViewGroup panel;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        ViewGroup panel = (ViewGroup) inflater.inflate(R.layout.pilihan_tambahan, container, false);
+        panel = (ViewGroup)inflater.inflate(R.layout.pilihan_tambahan,
+                container, false);
+        attachAksiPanelOnClickListener();
+        fillListViewPilihan();
+        return panel;
+    }
 
+    void attachAksiPanelOnClickListener() {
         FloatingActionButton aksiPanel = panel.findViewById(R.id.aksi_panel_tambah);
         aksiPanel.setOnClickListener(v -> {
             int origin = aksiPanel.getRotation() == 0 ? 0 : INDIKATOR_PANEL_ANGLE;
@@ -67,7 +68,9 @@ public class PilihanTambahan extends Fragment {
             });
             if (!hasStarted) aksiPanel.startAnimation(anim);
         });
+    }
 
+    void fillListViewPilihan() {
         ArrayList<String> deretPilihan = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             deretPilihan.add(String.valueOf((i + 1) * 1000));
@@ -82,7 +85,6 @@ public class PilihanTambahan extends Fragment {
         pilihan.setAdapter(adapter);
 
         pilihan.setOnItemClickListener((parent, view, position, id) -> selected = position);
-
         panel.findViewById(R.id.bayar).setOnClickListener(v -> {
             if (selected != -1) {
                 MainActivity activity = (MainActivity) getActivity();
@@ -92,7 +94,5 @@ public class PilihanTambahan extends Fragment {
                 activity.pembayaranDipilih(Integer.parseInt(dipilih));
             }
         });
-
-        return panel;
     }
 }
